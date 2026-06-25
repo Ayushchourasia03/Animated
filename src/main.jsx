35d9a925a2db3f20284059d1ed4e9b1d019ba85f
Ayush <ak4345230@gmail.com>
2026-06-26 01:37:00 +0530

    Add my file

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Jai ho</h1>
+    <App />
+  </React.StrictMode>,
+)
